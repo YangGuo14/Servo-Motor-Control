eec262be2db2ee960a5bbafc2fe8b57050bccf6a
YangGuo14 <98714679+YangGuo14@users.noreply.github.com>
2022-04-10 22:23:38 -0400

    Add files via upload

---
+++ Servo_Arduino/Servo.ino
@@ -0,0 +1,25 @@
+#include<Servo.h> //include the servo library
+Servo servo; //create a servo object
+int button1 = 6;   //pin of the first button
+int button2 = 7;  //pin of the second button
+int pos = 0;  //initial position
+void setup() {
+  // put your setup code here, to run once:
+  servo.attach(9);  //pin used by the servo motor
+  pinMode(button1, INPUT_PULLUP);  //define first button as input pullup
+  pinMode(button2, INPUT_PULLUP); //define second button as input pullup
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(button1) == LOW) {
+    pos++;  //increases the value of the "pos" variable each time the push button of the left is pressed
+    delay(5);
+    servo.write(pos); 
+  }
+  if (digitalRead(button2) == LOW) {
+    pos--;  //decreases the value of the "pos" variable each time the push button of the right is pressed
+    delay(5);
+    servo.write(pos);
+  }
+}
